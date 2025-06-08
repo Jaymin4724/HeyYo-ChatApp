1728e1245b8175a91f168908f445a3887146a258
jaymin dave <jaymin4724@gmail.com>
2025-06-08 16:31:01 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 // File Imports
 import ConnectDB from "./config/db.js";
@@ -9,9 +10,10 @@ import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import { app, server } from "./socket/socket.js";
+
 // Variables
-// const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 // Middleware
 app.use(express.json()); // Parse JSON bodies (from incoming requests - JSON to JS objects)
@@ -22,9 +24,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/messages", messageRoutes);
 
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
