daf35ec30ff8345b42074fa77ff23dedfd0f440d
jaymin dave <jaymin4724@gmail.com>
2025-06-08 21:18:02 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/messages", messageRoutes);
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
-app.get("*", (req, res) => {
+app.get("/*splat", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
